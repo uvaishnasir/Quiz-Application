652fcdfe80e43b3f4236b83c3a308ab5e616bda1
Uvaish <uvaish.nasir@handajitech.com>
2024-10-12 19:35:04 +0530

    register and login user controller added and tested

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/hello", (req, res) => {
-  res.send("Hello, World!");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("Hello, World!");
+// });
 
 //routes declaration
+import userRoutes from "./routes/user.routes.js";
+import quizRoutes from "./routes/quiz.routes.js";
+
+//mounting routes
+app.use("/api/users", userRoutes);
+app.use("/api/quiz", quizRoutes);
 
 export { app };
